3b2073701e12b630b81f19a4b976fa9ac5befbbf
T107 <T107@PC252>
2014-10-31 21:30:10 -0600

    listo

---
+++ HJMB/src/main/java/com/hm/model/Evaluacion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hm.model;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author T107
+ */
+public class Evaluacion {
+   
+    public String nombre;
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public float calificacion;
+
+    /**
+     * Set the value of calificacion
+     *
+     * @param calificacion new value of calificacion
+     */
+    public void setCalificacion(float calificacion) {
+        this.calificacion = calificacion;
+    }
+
+}
---
+++ HJMB/src/main/java/com/hm/model/GeneradorCalificaciones.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hm.model;
+
+/**
+ *
+ * @author T107
+ */
+public class GeneradorCalificaciones {
+    
+ public String nombre;
+    public double calificacion;
+    
+    public void generarCalificacion(String evaluacion) {
+        this.nombre = "Petra";
+        this.calificacion = 5.8;
+        this.nombre = "José";
+        this.calificacion = 7.4;
+        this.nombre = "Roberta";
+        this.calificacion = 8.6;
+    }
+}
